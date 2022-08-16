972fe4ee45b25a86dc76f6950e01f348c94dbada
Gabriel Villamarin Moreno <111314036+GaboVillaUNCol@users.noreply.github.com>
2022-08-16 10:14:43 -0500

    Add files via upload

---
+++ Reto_5G8/src/main/java/com/mycompany/reto_5g8/Controlador/ControladorMain.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.reto_5g8.Controlador;
+
+import com.mycompany.reto_5g8.Modelo.DAO.Conexion;
+
+import com.mycompany.reto_5g8.Vista.frmPrincipal2;
+import java.sql.Connection;
+
+public class ControladorMain {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new frmPrincipal2().setVisible(true);
+      
+       
+       //Conexion cc = new Conexion();
+      // Connection cn = cc.conectar();
+        
+    }
+    
+}
